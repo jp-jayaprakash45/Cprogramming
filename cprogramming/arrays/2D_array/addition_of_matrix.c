b3f5ca14e89acc498a2ed9e6bc703e40fc8e3b49
jayaprakash <polurujayaprakash@gmail.com>
2023-10-19 11:34:49 +0000

    addition of 2 matrices in 2D array

---
+++ cprogramming/arrays/2D_array/addition_of_matrix.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    
+    int a[2][3]={{1,2,3},{4,5,6}};
+    int b[2][3]={{1,2,3},{4,5,6}};
+    int c[2][3],i,j;
+    
+    for(i=0;i<2;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            c[i][j]=a[i][j] + b[i][j];
+            
+            printf("%d\t",c[i][j]);
+        }
+        
+        printf("\n");
+    }
+    
+    
+
+    return 0;
+}
