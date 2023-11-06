dc3668f2f8c1962eec18c1136b2179a77705f345
jayaprakash <polurujayaprakash@gmail.com>
2023-11-06 05:49:21 +0000

    pointers to array of strings

---
+++ cprogramming/pointers/basic_programs_using_pointers/array_of_string.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    char *a[]= { "Hello","World","Hieeee"};
+    
+    
+    for(int i=0;i<3 ;i++)
+    {
+        printf("%s\n",a[i]);
+    }
+
+    return 0;
+}
+
+
