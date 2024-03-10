38e97a0bb1c1b7613127d3264e397bcc25ca74f0
jayaprakash <polurujayaprakash@gmail.com>
2024-03-10 06:31:32 +0000

    sys_program using signals

---
+++ os-programming/Sys_programs/signal.c
@@ -0,0 +1,28 @@
+#include <signal.h>
+#include <stdio.h>
+#include <unistd.h>
+void catch_int(int sig_num) {
+  printf("Caught the SIGNINT i.e. Ctrl + C\n");
+  fflush(stdout);
+  return;
+}
+
+int main() {
+  signal(2, catch_int);
+  // signal(2, catch_int);
+  signal(9, SIG_IGN);
+
+  printf("Have a Great day\n");
+
+  while (1) {
+    printf("Happy Maha Shivratri\n");
+    sleep(1);
+  }
+
+  pause();
+
+  printf("Yeah Thank you\n");
+  printf("Yeahyy Thank you\n");
+
+  return 0;
+}
