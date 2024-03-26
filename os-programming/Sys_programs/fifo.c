0f2745576d87ff82b2dbed99686bab781050e1bc
jayaprakash <polurujayaprakash@gmail.com>
2024-03-26 05:08:13 +0000

    writing through fifo

---
+++ os-programming/Sys_programs/fifo.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+ #include<stdlib.h>
+ #include<fcntl.h>
+ #include<sys/stat.h>
+ #include<sys/types.h>
+ #include<string.h>
+ #include<unistd.h>
+
+int main(){
+        
+         if(mkfifo("mk_fifo",0666) == -1){
+           perror("mkfifo");
+          return -1;
+}
+        printf("successfully created the FIFO \n");
+
+   int fd;
+   fd =  open ("mk_fifo",O_WRONLY);
+
+    if(fd == -1){
+          perror("open");
+          exit(1);
+}
+char msg[100];
+
+   while(1){
+      printf(" Enter msg : ");
+     scanf("%s",msg);
+
+    write(fd ,msg,(size_t)(strlen(msg))+1);
+
+}
+}
+
+
+
